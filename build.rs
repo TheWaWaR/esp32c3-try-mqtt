a74f2b5eaae4305c09d43677172e27fa670781a7
Linfeng Qian <thewawar@gmail.com>
2023-02-15 23:30:08 +0800

    MQTT client

--- build.rs
+++ build.rs
@@ -1,6 +1,48 @@
+use std::{
+    env,
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
+
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
+
+    let out_path = Path::new(&env::var("OUT_DIR").unwrap()).join("envs.rs");
+    let mut out_file = BufWriter::new(File::create(&out_path).expect("create envs.rs"));
+    writeln!(
+        &mut out_file,
+        "pub const WIFI_SSID: &str = \"{}\";",
+        env!("ESP_WIFI_SSID"),
+    )
+    .expect("write to envs.rs");
+    writeln!(
+        &mut out_file,
+        "pub const WIFI_PASS: &str = \"{}\";",
+        env!("ESP_WIFI_PASS"),
+    )
+    .expect("write to envs.rs");
+
+    writeln!(
+        &mut out_file,
+        "pub const MQTT_HOST: &str = \"{}\";",
+        env!("ESP_MQTT_HOST"),
+    )
+    .expect("write to envs.rs");
+    writeln!(
+        &mut out_file,
+        "pub const MQTT_USER: &str = \"{}\";",
+        env!("ESP_MQTT_USER"),
+    )
+    .expect("write to envs.rs");
+    writeln!(
+        &mut out_file,
+        "pub const MQTT_PASS: &str = \"{}\";",
+        env!("ESP_MQTT_PASS"),
+    )
+    .expect("write to envs.rs");
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,112 @@
-use std::thread::sleep;
+use std::net::Ipv4Addr;
+use std::thread::{self, sleep};
 use std::time::Duration;
 
 use esp_idf_hal::ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver};
+use esp_idf_hal::modem::WifiModem;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
 
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+use embedded_svc::mqtt::client::{Client, Connection, Event, Message, Publish, QoS};
+use embedded_svc::wifi::{ClientConfiguration, Configuration};
+
+use esp_idf_svc::mqtt::client::{EspMqttClient, MqttClientConfiguration};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
+
+include!(concat!(env!("OUT_DIR"), "/envs.rs"));
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
-    println!("Hello, world!");
-
-    let peripherals = Peripherals::take().unwrap();
+    let peripherals = Peripherals::take().expect("peripherals");
     let timer_config = TimerConfig::default().frequency(1.kHz().into());
-    let timer_driver0 = LedcTimerDriver::new(peripherals.ledc.timer0, &timer_config).unwrap();
-    let timer_driver1 = LedcTimerDriver::new(peripherals.ledc.timer1, &timer_config).unwrap();
+    let timer_driver0 =
+        LedcTimerDriver::new(peripherals.ledc.timer0, &timer_config).expect("create timer 0");
+    let timer_driver1 =
+        LedcTimerDriver::new(peripherals.ledc.timer1, &timer_config).expect("create timer 0");
     let mut driver4 = LedcDriver::new(
         peripherals.ledc.channel0,
         timer_driver0,
         peripherals.pins.gpio12,
         &timer_config,
     )
-    .unwrap();
+    .expect("create driver4");
     let mut driver5 = LedcDriver::new(
         peripherals.ledc.channel1,
         timer_driver1,
         peripherals.pins.gpio13,
         &timer_config,
     )
+    .expect("create driver5");
+
+    // ==== Wifi ====
+    let mut wifi = EspWifi::new(
+        unsafe { WifiModem::new() },
+        EspSystemEventLoop::take().unwrap(),
+        Some(EspDefaultNvsPartition::take().unwrap()),
+    )
     .unwrap();
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: WIFI_SSID.into(),
+        password: WIFI_PASS.into(),
+        ..Default::default()
+    }))
+    .unwrap();
+    wifi.start().unwrap();
+    wifi.scan().unwrap();
+    wifi.connect().unwrap();
+    let netif = wifi.sta_netif();
+    println!("Waiting for wifi......");
+    loop {
+        sleep(Duration::from_millis(200));
+        let info = netif.get_ip_info().unwrap();
+        if info.ip != Ipv4Addr::new(0, 0, 0, 0) {
+            break;
+        }
+    }
+    let mac = netif.get_mac().unwrap();
+    let ip = netif.get_ip_info().unwrap();
+    println!("wifi mac: {:?}, ip: {:?}", mac, ip);
+
+    // ==== MQTT ====
+    let mqtt_conf = MqttClientConfiguration {
+        client_id: Some("my-esp32"),
+        network_timeout: Duration::from_secs(5),
+        username: Some(MQTT_USER),
+        password: Some(MQTT_PASS),
+        ..Default::default()
+    };
+    println!("Connecting to MQTT broker....");
+    let (mut mqtt_client, mut mqtt_conn) =
+        EspMqttClient::new_with_conn(format!("mqtt://{}", MQTT_HOST), &mqtt_conf).unwrap();
+    thread::spawn(move || {
+        println!("MQTT Listening for messages");
+        while let Some(msg) = mqtt_conn.next() {
+            match msg {
+                Err(e) => println!("MQTT Message ERROR: {}", e),
+                Ok(event) => {
+                    println!("MQTT Event: {:?}", event);
+                    // Wait for a received event
+                    match event {
+                        Event::Received(rcv) => {
+                            let s = match std::str::from_utf8(rcv.data()) {
+                                Ok(v) => v,
+                                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                            };
+                            println!("rcv: {:?}", s);
+                        }
+                        // Ignore all other events for now
+                        _ => {}
+                    }
+                }
+            }
+        }
+        println!("MQTT connection loop exit");
+    });
 
     const POW: u32 = 2;
     const TOTAL: u32 = 100;
@@ -53,14 +128,24 @@ fn main() {
         }
         let duty4 = map_duty(max_duty, duty_parts);
         let duty5 = map_duty(max_duty, TOTAL - duty_parts);
-        driver4.set_duty(duty4).unwrap();
-        driver5.set_duty(duty5).unwrap();
+        driver4.set_duty(duty4).expect("set duty4");
+        driver5.set_duty(duty5).expect("set duty5");
         sleep(Duration::from_millis(20));
-        if i % 5 == 0 {
+        if i % 25 == 0 {
             println!(
                 "> tick #{:003}, parts: {:003}, duty4: {:003}, duty5: {:003}",
-                i, duty_parts, duty4 as u32, duty5 as u32,
+                i, duty_parts, duty4, duty5,
             );
+        }
+        if i % 100 == 0 {
+            mqtt_client
+                .publish(
+                    "testtopic/thewawar/esp32c3",
+                    QoS::AtMostOnce,
+                    false,
+                    format!("D4.duty: {}, D5.duty: {}", duty4, duty5).as_bytes(),
+                )
+                .unwrap();
         }
     }
 }
